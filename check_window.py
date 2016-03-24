f7113eb4643a8825e511535602f3f29e14c2f726
Sasori <345563880@qq.com>
2016-03-24 14:33:24 +0800

    Update: 使用event_generate解决文件检索是窗体卡死的问题

--- check_window.py
+++ check_window.py
@@ -5,12 +5,13 @@ import Tkinter
 import tkFileDialog
 import tkMessageBox
 import file_scan
+import threading
 
 
 top = Tkinter.Tk()
 top.title(u'文件内容检索')
-top.minsize(500, 200)
-top.maxsize(800, 600)
+top.minsize(500, 400)
+top.maxsize(500, 600)
 
 
 lbl1 = Tkinter.StringVar()
@@ -27,28 +28,41 @@ text1.place(x=100, y=17, width=310, height=20)
 text2 = Tkinter.Text(top, relief=Tkinter.SOLID)
 text2.place(x=100, y=67, width=310, height=20)
 
+text3 = Tkinter.Text(top, relief=Tkinter.SOLID)
+text3.place(x=100, y=107, width=310, height=260)
+
 def choose_path():
     file_path = tkFileDialog.askdirectory()
     text1.delete(u"1.0", Tkinter.END)  # delete the content of Text
     text1.insert(Tkinter.INSERT, file_path)
 
 def start_searching():
+    global file_path
     file_path = text1.get(1.0, u"end-1c")  # get the content of Text
-    # text1.get(1.0, Tkinter.END)   这样获取的内容会多出一个空行
-    print file_path
+    # print text1.get(1.0, Tkinter.END)  # 这样获取的内容会多出一个空行
     if file_path == '':
         tkMessageBox.showinfo(u"提示", u"请选择检索的文件路径")
+        return
     else:
+        global keyword
         keyword = text2.get(1.0, u'end-1c')
         if keyword == '':
             tkMessageBox.showinfo(u"提示", u"请输入要检索的关键字")
+            return
+        com_thread = threading.Thread(target=get_file_path)
+        com_thread.start()
+
+def get_file_path():
+    global Text
     included_files = file_scan.keyword_check(file_path, keyword)
-    print '\r\n'.join(included_files)
+    Text = '\r\n'.join(included_files)
+    top.event_generate("<<GETFILELIST>>", when="tail")
+
+top.bind("<<GETFILELIST>>", lambda evt: text3.insert("insert", Text))
+
 
 btn1 = Tkinter.Button(top, text=u"  选  择  ", command=choose_path).place(x=420, y=10)
 btn2 = Tkinter.Button(top, text=u"开始检索", command=start_searching).place(x=420, y=60)
 
 
 top.mainloop()
-
-
